fb95bf32c5ebdb8ac78584d43f09edc028cd084b
skibidip1 <vgg8862@gmail.com>
2025-09-06 13:32:28 +0700

    Update Bloxfruits.lua

--- Bloxfruits.lua
+++ Bloxfruits.lua
@@ -2,6 +2,6 @@ getgenv().setting = {
     ["team"] = "Marines", -- Pirates, Marines
     ["V4"] = true, -- true/false
     ["FPS"] = true, -- true/false
-    ["name"] = "Doughking" -- Rip_indra, Darkbread, Doughking, Tyrant, Cursed_captian
+    ["name"] = "Rip_indra" -- Rip_indra, Darkbread, Doughking, Tyrant, Cursed_captian
 }
 loadstring(game:HttpGet("https://raw.githubusercontent.com/skibidip1/Hack/refs/heads/main/Premium"))()
